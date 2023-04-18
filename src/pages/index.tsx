1970833e1d41df0889e32ea95af90707665a49e6
Ruslan Musaev <ruslan.msv@gmail.com>
2023-04-18 15:06:37 +0200

    Larget font for content text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -57,7 +57,7 @@ const PostView = (props: PostWithUser) => {
           &nbsp;·&nbsp;
           <span className="font-thin">{dayjs(post.createdAt).fromNow()}</span>
         </div>
-        <span>{post.content}</span>
+        <span className="text-2xl">{post.content}</span>
       </div>
     </div>
   );
